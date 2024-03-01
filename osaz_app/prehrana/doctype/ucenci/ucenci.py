1370b712768cb7660fce2c7b093e8ad2ffee361a
sp9leo <sp9leonardo@gmail.com>
2024-03-01 11:18:04 +0000

    qr name change

--- osaz_app/prehrana/doctype/ucenci/ucenci.py
+++ osaz_app/prehrana/doctype/ucenci/ucenci.py
@@ -18,11 +18,14 @@ class Ucenci(Document):
 		d=str(self.vozac).replace("1","V")
 		e=str(self.dieta).replace("1","D")
 		n=str(self.name)
+		o=str(self.oddelek)
 		#name = random_string(12)+"000"+ a + b + c + d +e
 		name = n+"000"+ a + b + c + d +e
 		name = str(name)
+		qrcode = str(n+"0"+o)
 		self.ucenec_id = name
-		self.qr_code = get_qr_code(self.ucenec_id)
+		self.qr_code = get_qr_code(qrcode)
+		#self.qr_code = get_qr_code(self.ucenec_id)
 		#self.ucenec_id = get_qr_code(self.<field>)
 
 		
